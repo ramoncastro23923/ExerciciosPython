8081a985e7dc6a5ec5f67eac72b06e50127e0f3e
Ramon <ramoncastrovieira@gmail.com>
2025-08-13 16:16:27 -0300

    começo fromKEYS

--- Coleções/Dicionários/dicionarios6.py
+++ Coleções/Dicionários/dicionarios6.py
@@ -11,8 +11,31 @@ dados["nome"] = "Pedro"
 dados["idade"] = "32"
 
 #A função popitem elimina o último item apenas da versão 3.7 e acima
-dados.popitem()
+# dados.popitem()
+print(dados)
+# dados.pop("valor_logico")
+print(dados)
+# del dados["ano"]
+print(dados)
+# dados.clear()
 print(dados)
 
 # dados.update({"estado", "Rio de Janeiro"})
 # dados.update({"Nome", "Ana"})   #Não é possível atualizar com .update por ser um dicionário
+
+for x in dados.values():
+    print(x)
+
+for x,y in dados.items():
+    print(x, y)
+
+dicio = dados.copy()
+print(dicio)
+
+dicio2 = dict(dados)
+print(dicio2)
+
+dados["idade"] = 27
+print(dados)
+print(dicio)
+print(dicio2)
---
+++ Coleções/Dicionários/funcoesFromKEYS.py
@@ -0,0 +1,3 @@
+#index      0      1    2
+tupla = ("item1", "item2", "item3")
+dicio = dict(tupla)
